a29da7a76272c32d2f814f7ad3c723768fd23c19
Kitsune1150 <dhdcghv@gmail.com>
2025-12-14 22:43:06 +0700

    lab03

---
+++ docs/lab03_typescript/dist/app.js
@@ -0,0 +1,73 @@
+import { StudentManager } from "./managers/StudentManager.js";
+import { showList } from "./utils/showList.js";
+const manager = new StudentManager();
+manager.loadFromLocalStorage();
+function renderTable(elementId = "studentTableBody") {
+    const tableBody = document.getElementById(elementId);
+    tableBody.innerHTML = "";
+    const students = manager.getAllStudents();
+    showList(students);
+    students.forEach((s) => {
+        tableBody.innerHTML += `
+      <tr>
+        <td>${s.id}</td>
+        <!-- 🔴 แก้ไข -->
+        <td>${s.title_name}</td>
+        <td>${s.first_name}</td>
+        <td>${s.last_name}</td>
+        <td>${s.email}</td>
+        <!-- 🔴 /แก้ไข -->
+        <td>${s.year}</td>
+        <td>${s.major}</td>
+      </tr>
+    `;
+    });
+}
+document.getElementById("addBtn").onclick = () => {
+    const id = document.getElementById("id").value;
+    const year = Number(document.getElementById("year").value);
+    const major = document.getElementById("major").value;
+    // 🔴 แก้ไข
+    const title_name = document.getElementById("title_name").value;
+    const first_name = document.getElementById("first_name").value;
+    const last_name = document.getElementById("last_name").value;
+    const email = document.getElementById("email").value;
+    // 🔴 /แก้ไข
+    const student = {
+        id,
+        title_name,
+        first_name,
+        last_name,
+        email,
+        year,
+        major,
+    };
+    manager.addStudent(student);
+    renderTable();
+};
+document.getElementById("searchNameBtn").onclick = () => {
+    const keyword = document.getElementById("searchName").value;
+    const results = manager.findStudentsByName(keyword);
+    showList(results);
+    alert(`ผลการค้นหา: ${results.length} คน`);
+};
+document.getElementById("searchMajorBtn").onclick = () => {
+    const keyword = document.getElementById("searchMajor").value;
+    const results = manager.findStudentsByMajor(keyword);
+    showList(results);
+    alert(`พบในสาขา: ${results.length} คน`);
+};
+// 🔴 เพิ่ม ค้นหาด้วย Email
+document.getElementById("searchEmailBtn").onclick = () => {
+    const email = document.getElementById("searchEmail").value;
+    const result = manager.findStudentByEmail(email);
+    if (result) {
+        showList([result]);
+        alert(`พบอีเมล: ${result.first_name} ${result.last_name}`);
+    }
+    else {
+        alert("ไม่พบอีเมลนี้");
+    }
+};
+// 🔴 /เพิ่ม
+renderTable("studentTableBody");
---
+++ docs/lab03_typescript/src/models/Student.ts
@@ -0,0 +1,9 @@
+export interface Student {
+  title_name: string;
+  first_name: string;
+  last_name: string;
+  year: number;
+  major: string;
+  email: string;
+  id: string;
+}
